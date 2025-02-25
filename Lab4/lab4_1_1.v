fdc18de8c63848a2e14072cdff0022a38dbc0d72
LeviF <electrosquib@gmail.com>
2025-02-25 13:54:50 -1000

    Finish lab4_1_1.v

---
+++ Lab4/lab4_1_1.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+
+module add_two_values_task (
+    input [3:0] ain,
+    input [3:0] bin,
+    output reg cout,
+    output reg [3:0] sum
+);
+    parameter MAXBITS = 4;
+    task add;
+        input [MAXBITS - 1:0] a;
+        input [MAXBITS - 1:0] b;
+        output [MAXBITS - 1:0] sum;
+        output carry;
+
+        assign {carry, sum} = a + b;
+    endtask
+
+    always @ (ain or bin)
+        add(ain, bin, sum, cout);
+
+endmodule
